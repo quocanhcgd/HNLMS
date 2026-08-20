b7b9304675c482667c01e483644d4a9254e79b7e
quocanhcgd <quocanhcgd@gmail.com>
2026-08-20 19:50:52 +0700

    chore: exclude generated workspace builds

--- packages/theme-presets/dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.validateRequiredTokens = validateRequiredTokens;
-function validateRequiredTokens(tokens) {
-    const required = ["background", "foreground", "surface", "surfaceMuted", "primary", "border", "ring", "success", "warning", "danger", "info"];
-    return required.filter((key) => !tokens[key]);
-}
--- worker/dist/main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const startedAt = new Date().toISOString();
-console.log(JSON.stringify({ service: "worker", status: "ready", startedAt }));
-setInterval(() => undefined, 60_000);
